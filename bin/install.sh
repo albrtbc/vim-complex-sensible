a9e7681088b36dccc255c4373eaf4762f2609e3f
Albert Blanco-Cuaresma <bertobc@users.noreply.github.com>
2017-12-16 15:51:42 +0100

    = Fixed install script

--- bin/install.sh
+++ bin/install.sh
@@ -14,7 +14,7 @@ mkdir -p $HOME/.vim/autoload/
 curl -o $HOME/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -o $HOME/.vim/vimrc https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/vimrc
 curl -o $HOME/.gitconfig https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.gitconfig
-curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.tmux.conf.linux .tmux.conf
-#curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.tmux.conf.windows .tmux.conf
+curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.tmux.conf.linux
+#curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.tmux.conf.windows
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 vim +"PlugInstall 1" +qall
