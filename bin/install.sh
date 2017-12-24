ec30a0914e92c9b4b0499d2883b8f3a12f89bec7
Albert Blanco-Cuaresma <bertobc@users.noreply.github.com>
2017-12-24 21:01:44 +0100

    = fixed replace symbol

--- bin/install.sh
+++ bin/install.sh
@@ -9,6 +9,5 @@ curl -o $HOME/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/
 curl -o $HOME/.vim/vimrc https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/vimrc
 curl -o $HOME/.gitconfig https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.gitconfig
 curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.tmux.conf.linux
-#curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.tmux.conf.windows
-ln -s $HOME/.vim/vimrc $HOME/.vimrc
+ln -sf $HOME/.vim/vimrc $HOME/.vimrc
 vim +"PlugInstall 1" +qall
