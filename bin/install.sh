9f30bb401d7f81e410c102f0a68eb086c3282042
Albert Blanco-Cuaresma <bertobc@users.noreply.github.com>
2017-12-24 22:57:16 +0100

    + checks environment before install

--- bin/install.sh
+++ bin/install.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 set -u
 set -e
-
 set -x
+
 mkdir -p $HOME/.vim/undodir/
 mkdir -p $HOME/.vim/autoload/
 curl -o $HOME/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -o $HOME/.vim/vimrc https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/vimrc
 curl -o $HOME/.gitconfig https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.gitconfig
-curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.tmux.conf.linux
+if grep -q Microsoft /proc/version; then
+  echo "Ubuntu on Windows"
+  curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.tmux.conf.windows
+else
+  echo "native Linux"
+  curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.tmux.conf.linux
+fi
 ln -sf $HOME/.vim/vimrc $HOME/.vimrc
 if [ -d "${HOME}/.vim/" ] || [ -f "${HOME}/.vimrc" ] ; then
   echo "There is already a vim configuration in place! PlugUpdate:"
