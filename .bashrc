96920cacc08aa7cc7b70a80bff5eea927572c9b5
Albert Blanco-Cuaresma <albrtbc@users.noreply.github.com>
2019-04-08 17:49:23 +0200

    = url username

--- .bashrc
+++ .bashrc
@@ -110,7 +110,7 @@ alias l='ls -CF'
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\')"'
 
 # Alias definitions.
 # You may want to put all your additions into a separate file like
@@ -137,3 +137,6 @@ fi
 # enable bash-git-prompt
 #GIT_PROMPT_ONLY_IN_REPO=1
 #source ~/.bash-git-prompt/gitprompt.sh
+
+# slack-term                                                                     
+alias slack='slack-term --config $HOME/snap/slack-term/current/slack-term.json'  
--- bin/install.sh
+++ bin/install.sh
@@ -6,15 +6,15 @@ set -x
 mkdir -p $HOME/.vim/undodir/
 mkdir -p $HOME/.vim/autoload/
 curl -o $HOME/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -o $HOME/.vim/vimrc https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/vimrc
-curl -o $HOME/.gitconfig https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.gitconfig
-curl -o $HOME/.bashrc https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.bashrc
+curl -o $HOME/.vim/vimrc https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/vimrc
+curl -o $HOME/.gitconfig https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/.gitconfig
+curl -o $HOME/.bashrc https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/.bashrc
 if grep -q Microsoft /proc/version; then
   echo "Ubuntu on Windows"
-  curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.tmux.conf.windows
+  curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/.tmux.conf.windows
 else
   echo "native Linux"
-  curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/bertobc/vim-complex-sensible/master/.tmux.conf.linux
+  curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/.tmux.conf.linux
 fi
 ln -sf $HOME/.vim/vimrc $HOME/.vimrc
 if [ -d "${HOME}/.vim/" ] || [ -f "${HOME}/.vimrc" ] ; then
@@ -25,3 +25,5 @@ else
   echo "New installation! PlugInstall:"
   vim +"PlugInstall" +qall
 fi
+
+apt-get install vim tmux slack-term -y
