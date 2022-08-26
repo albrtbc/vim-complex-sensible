56fc317128134f4825572d45fcd1d7fd91843ba9
Albert Blanco-Cuaresma <albrtbc@users.noreply.github.com>
2022-08-26 19:26:32 +0200

    Separated vimrc files for win/unix systems

--- bin/install.sh
+++ bin/install.sh
@@ -9,13 +9,8 @@ curl -o $HOME/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/
 curl -o $HOME/.vim/vimrc https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/vimrc
 curl -o $HOME/.gitconfig https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/.gitconfig
 curl -o $HOME/.bashrc https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/.bashrc
-if grep -q Microsoft /proc/version; then
-  echo "Ubuntu on Windows"
-  curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/.tmux.conf.windows
-else
-  echo "native Linux"
-  curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/.tmux.conf.linux
-fi
+curl -o $HOME/.tmux.conf https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/.tmux.conf.linux
+#fi
 ln -sf $HOME/.vim/vimrc $HOME/.vimrc
 if [ -d "${HOME}/.vim/" ] || [ -f "${HOME}/.vimrc" ] ; then
   echo "There is already a vim configuration in place! PlugUpdate:"
@@ -28,10 +23,14 @@ fi
 
 if [ $(uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip') ];
 then
-    echo "Installing win32yank - clip sharing between wsl and windows"
     # clip shared for windows
+    echo "Installing win32yank - clip sharing between wsl and windows"
     curl -sLo /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
     unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
     chmod +x /tmp/win32yank.exe
     sudo mv /tmp/win32yank.exe /usr/local/bin/
+    # append to vimrc
+    echo "Append vimrc.windows functions into vimrc"
+    curl -o $HOME/.vim/vimrc.windows https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/vimrc.windows
+    cat $HOME/.vim/vimrc $HOME/.vim/vimrc.windows > $HOME/.vim/vimrc
 fi
