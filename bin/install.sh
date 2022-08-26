79743c641a912e4a1b4ca389ee91f064105c146c
Albert Blanco-Cuaresma <albrtbc@users.noreply.github.com>
2022-08-26 19:53:24 +0200

    remove sudo from inside the file

--- bin/install.sh
+++ bin/install.sh
@@ -23,14 +23,14 @@ fi
 
 if [ -d "/run/WSL" ];
 then
+    # append to vimrc
+    echo "Append vimrc.windows functions into vimrc"
+    curl -o $HOME/.vim/vimrc.windows https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/vimrc.windows
+    cat $HOME/.vim/vimrc $HOME/.vim/vimrc.windows > $HOME/.vim/vimrc
     # clip shared for windows
     echo "Installing win32yank - clip sharing between wsl and windows"
     curl -sLo /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
     unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
     chmod +x /tmp/win32yank.exe
-    sudo mv /tmp/win32yank.exe /usr/local/bin/
-    # append to vimrc
-    echo "Append vimrc.windows functions into vimrc"
-    curl -o $HOME/.vim/vimrc.windows https://raw.githubusercontent.com/albrtbc/vim-complex-sensible/master/vimrc.windows
-    cat $HOME/.vim/vimrc $HOME/.vim/vimrc.windows > $HOME/.vim/vimrc
+    mv /tmp/win32yank.exe /usr/local/bin/
 fi
